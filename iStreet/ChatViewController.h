473dba759d5095f27d2ecf0b8f0956e58ba146f9
Rishi Narang <rishinarang15@gmail.com>
2012-03-26 23:09:07 -0400

    Removed all the changes from test commits.

--- iStreet/ChatViewController.h
+++ iStreet/ChatViewController.h
@@ -5,12 +5,6 @@
 //  Created by Rishi on 3/18/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-// HELLO TEST COMMENT. FUCK YOU MERGE!
-// RAWR
-
-// conflict!
-
-// test 2
 
 #import <UIKit/UIKit.h>
 
