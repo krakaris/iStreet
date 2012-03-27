2b665ca9fd9fd1452edf1e97b3cc50778bbea721
Rishi Narang <rishinarang15@gmail.com>
2012-03-26 21:51:09 -0400

    Test commit / fixing aki's horrible errors

--- iStreet/HomeViewController.h
+++ iStreet/HomeViewController.h
@@ -7,24 +7,8 @@
 //
 
 
-//BAHAHAHAHAHAHA
-
-//WHAT AERIAJDCLKNSV:J ASDF:LKJASD:LAJSD:
-
-//ADLkjSD KJHAD LKJDHSLKAJHSDLKAJSDH  ASDJHLASDKJLSADKJ
-
-//Well this was just great
-
-//And this was just even more awesome. Aww yeahhh this is how we do it ;)
-
-//And finally, this takes the cake :D
-
-//And now I'm finally gonna call it. Git is going to remember my password and I will FINALLY sleep a happy man. for 8 hours. TROLLOLOL
-
 #import <UIKit/UIKit.h>
 
 @interface HomeViewController : UIViewController
-
-@property (nonatomic, retain) NSString *watup;
 
 @end
